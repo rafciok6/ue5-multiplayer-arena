db5800585420cdfd4f4cbf02b3a5f7f64e995c4d
Rafal <wanowiczrafal@gmail.com>
2026-07-13 15:17:08 +0200

    Added arena match timer.

---
+++ MultiplayerArena/Source/MultiplayerArena/Arena/ArenaPlayerController.cpp
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ArenaPlayerController.h"
+#include "Blueprint/UserWidget.h"
+
+void AArenaPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ShowArenaHUD();
+}
+
+void AArenaPlayerController::PostSeamlessTravel()
+{
+	Super::PostSeamlessTravel();
+
+	ShowArenaHUD();
+}
+
+void AArenaPlayerController::EndPlay(
+	const EEndPlayReason::Type EndPlayReason)
+{
+	RemoveArenaHUD();
+
+	Super::EndPlay(EndPlayReason);
+}
+
+void AArenaPlayerController::ShowArenaHUD()
+{
+	if (!IsLocalController() ||	!ArenaHUDWidgetClass ||	IsValid(ArenaHUDWidget))
+	{
+		return;
+	}
+
+	ArenaHUDWidget = CreateWidget<UUserWidget>(this, ArenaHUDWidgetClass);
+
+	if (!IsValid(ArenaHUDWidget))
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to create arena HUD"));
+		return;
+	}
+
+	ArenaHUDWidget->AddToPlayerScreen();
+
+	UE_LOG(LogTemp, Log, TEXT("Arena HUD added to player screen"));
+}
+
+void AArenaPlayerController::RemoveArenaHUD()
+{
+	if (!IsValid(ArenaHUDWidget))
+	{
+		return;
+	}
+
+	ArenaHUDWidget->RemoveFromParent();
+	ArenaHUDWidget = nullptr;
+}
---
+++ MultiplayerArena/Source/MultiplayerArena/Arena/ArenaPlayerController.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MultiplayerArena/MultiplayerArenaPlayerController.h"
+#include "ArenaPlayerController.generated.h"
+
+/**
+ * 
+*/
+class UUserWidget;
+
+UCLASS()
+class MULTIPLAYERARENA_API AArenaPlayerController : public AMultiplayerArenaPlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void PostSeamlessTravel() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Arena|UI")
+	TSubclassOf<UUserWidget> ArenaHUDWidgetClass;
+
+private:
+	void ShowArenaHUD();
+	void RemoveArenaHUD();
+
+	UPROPERTY()
+	TObjectPtr<UUserWidget> ArenaHUDWidget;
+};
