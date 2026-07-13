81c5a23cbe7047d9914d804e5139c8d6a7af1eaa
Rafal <wanowiczrafal@gmail.com>
2026-07-13 20:32:00 +0200

    Added server hitscan combat, created ArenaCharacter.

---
+++ MultiplayerArena/Source/MultiplayerArena/Arena/ArenaCharacter.cpp
@@ -0,0 +1,174 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ArenaCharacter.h"
+#include "Net/UnrealNetwork.h"
+#include "ArenaGameState.h"
+#include "EnhancedInputComponent.h"
+#include "Engine/World.h"
+#include "GameFramework/DamageType.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/CapsuleComponent.h"
+
+AArenaCharacter::AArenaCharacter()
+{
+	bReplicates = true;
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Visibility,ECR_Block);
+}
+
+void AArenaCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority())
+	{
+		CurrentHealth = MaxHealth;
+		NotifyHealthChanged();
+		ForceNetUpdate();
+	}
+}
+
+void AArenaCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent =Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (FireAction)
+		{
+			EnhancedInputComponent->BindAction(FireAction,ETriggerEvent::Started,this, &AArenaCharacter::Fire);
+		}
+	}
+}
+
+float AArenaCharacter::TakeDamage(float DamageAmount, const FDamageEvent& DamageEvent, AController* EventInstigator, AActor* DamageCauser)
+{
+	if (!HasAuthority() || bIsDead || DamageAmount <= 0.0f)
+	{
+		return 0.0f;
+	}
+
+	const float AppliedDamage = Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+
+	if (AppliedDamage <= 0.0f)
+	{
+		return 0.0f;
+	}
+
+	CurrentHealth = FMath::Clamp(CurrentHealth - AppliedDamage,0.0f, MaxHealth);
+
+	NotifyHealthChanged();
+	ForceNetUpdate();
+
+	if (FMath::IsNearlyZero(CurrentHealth))
+	{
+		bIsDead = true;
+		HandleDeath(EventInstigator);
+	}
+
+	return AppliedDamage;
+}
+
+void AArenaCharacter::OnRep_Health()
+{
+	NotifyHealthChanged();
+}
+
+void AArenaCharacter::NotifyHealthChanged()
+{
+	OnHealthChanged.Broadcast(CurrentHealth, MaxHealth);
+}
+
+void AArenaCharacter::HandleDeath(AController* KillerController)
+{
+	UE_LOG(
+		LogTemp,
+		Log,
+		TEXT("Arena character %s died"),
+		*GetName());
+}
+
+void AArenaCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AArenaCharacter, CurrentHealth);
+}
+
+void AArenaCharacter::Fire()
+{
+	if (!IsLocallyControlled())
+	{
+		return;
+	}
+
+	ServerFire();
+}
+
+void AArenaCharacter::ServerFire_Implementation()
+{
+	UWorld* World = GetWorld();
+
+	if (!IsValid(World) || !IsValid(Controller) || bIsDead)
+	{
+		return;
+	}
+
+	if (const AArenaGameState* GameState = World->GetGameState<AArenaGameState>())
+	{
+		if (GameState->IsMatchFinished())
+		{
+			return;
+		}
+	}
+
+	const float CurrentTime = World->GetTimeSeconds();
+	if (CurrentTime - LastFireTime < FireInterval)
+	{
+		return;
+	}
+
+	LastFireTime = CurrentTime;
+
+	FVector ViewLocation;
+	FRotator ViewRotation;
+	Controller->GetPlayerViewPoint(ViewLocation, ViewRotation);
+
+	const FVector TraceDirection = ViewRotation.Vector();
+	const FVector TraceEnd = ViewLocation + TraceDirection * FireRange;
+
+	FCollisionQueryParams QueryParams(SCENE_QUERY_STAT(ArenaFireTrace),false,this);
+
+	QueryParams.AddIgnoredActor(this);
+
+	FHitResult HitResult;
+	const bool bHit = World->LineTraceSingleByChannel(HitResult,	ViewLocation,	TraceEnd,ECC_Visibility,	QueryParams);
+	const FVector EffectEnd = bHit ? HitResult.ImpactPoint : TraceEnd;
+
+	MulticastPlayFireEffects(ViewLocation, EffectEnd);
+	
+	AArenaCharacter* HitCharacter = Cast<AArenaCharacter>(HitResult.GetActor());
+
+	if (!bHit || !IsValid(HitCharacter))
+	{
+		return;
+	}
+
+	const float AppliedDamage = UGameplayStatics::ApplyPointDamage(HitCharacter, FireDamage, TraceDirection,HitResult,Controller,this, UDamageType::StaticClass());
+
+	if (AppliedDamage > 0.0f)
+	{
+		UE_LOG(
+			LogTemp,
+			Log,
+			TEXT("%s hit %s for %.0f damage"),
+			*GetName(),
+			*HitCharacter->GetName(),
+			AppliedDamage);
+	}
+}
+
+void AArenaCharacter::MulticastPlayFireEffects_Implementation(FVector_NetQuantize TraceStart, FVector_NetQuantize TraceEnd)
+{
+	PlayFireEffects(TraceStart, TraceEnd);
+}
---
+++ MultiplayerArena/Source/MultiplayerArena/Arena/ArenaCharacter.h
@@ -0,0 +1,89 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MultiplayerArena/MultiplayerArenaCharacter.h"
+#include "ArenaCharacter.generated.h"
+
+class UInputAction;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FOnHealthChanged, float, CurrentHealth, float, MaxHealth);
+
+UCLASS()
+class MULTIPLAYERARENA_API AArenaCharacter : public AMultiplayerArenaCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AArenaCharacter();
+
+	virtual float TakeDamage(float DamageAmount,const FDamageEvent& DamageEvent,AController* EventInstigator,AActor* DamageCauser) override;
+
+	UFUNCTION(BlueprintPure, Category = "Combat")
+	float GetCurrentHealth() const
+	{
+		return CurrentHealth;
+	}
+
+	UFUNCTION(BlueprintPure, Category = "Combat")
+	float GetMaxHealth() const
+	{
+		return MaxHealth;
+	}
+
+	UFUNCTION(BlueprintPure, Category = "Combat")
+	float GetHealthPercent() const
+	{
+		return MaxHealth > 0.0f ? CurrentHealth / MaxHealth : 0.0f;
+	}
+
+	UPROPERTY(BlueprintAssignable, Category = "Combat")
+	FOnHealthChanged OnHealthChanged;
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	TObjectPtr<UInputAction> FireAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat|Weapon", meta = (ClampMin = "0.0"))
+	float FireDamage = 34.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat|Weapon", meta = (ClampMin = "100.0"))
+	float FireRange = 5000.0f;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat|Weapon", meta = (ClampMin = "0.1"))
+	float FireInterval = 0.25f;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Combat|Weapon")
+	void PlayFireEffects(FVector TraceStart, FVector TraceEnd);
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Combat", meta = (ClampMin = "1.0"))
+	float MaxHealth = 100.0f;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Health, BlueprintReadOnly, Category = "Combat")
+	float CurrentHealth = 0.0f;
+
+private:
+	UFUNCTION()
+	void OnRep_Health();
+
+	void NotifyHealthChanged();
+	void HandleDeath(AController* KillerController);
+
+	void Fire();
+
+	UFUNCTION(Server, Reliable)
+	void ServerFire();
+
+	UFUNCTION(NetMulticast, Unreliable)
+	void MulticastPlayFireEffects(FVector_NetQuantize TraceStart, FVector_NetQuantize TraceEnd);
+
+	bool bIsDead = false;
+	float LastFireTime = -1000.0f;
+};
